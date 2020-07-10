9a6d3a63548da5b8080e9b8b9af734e7fcdd1921
SachaCamiloGarcia <62697403+SachaCamiloGarcia@users.noreply.github.com>
2020-07-10 00:22:07 -0300

    Update filecreator.cpp

--- filecreator.cpp
+++ filecreator.cpp
@@ -1,6 +1,17 @@
+// Este codigo deberia servir para compilar un .exe que permita ingresar datos por consola y imprimirlos en "Paises.txt" y "ParteDiario.txt" 
+
+
 #include <iostream>
 #include <string>
 #include<fstream>
+
+// #define variables para funcion "std::string CalculateSpaces(const std::string& buffer, PARAMETROS type)"
+#define SIZE_NOMBRE = 20
+#define SIZE_CONTINENTE = 10
+#define SIZE_HABITANTES = 9
+#define SIZE_FECHA = 9
+#define SIZE_CANTIDAD = 9
+
 
 // Stream de archivo global
 std::fstream file;
@@ -9,7 +20,9 @@ enum PARAMETROS
 {
 	NOMBRE,
 	CONTINENTE,
-	HABITANTES
+	HABITANTES,
+	FECHA,
+	CANTIDAD
 };
 
 void OpenFile(std::fstream& file, std::string nameFile) 	// Funcion para abrir un archivo 
@@ -36,13 +49,19 @@ std::string CalculateSpaces(const std::string& buffer, PARAMETROS type) // Funci
 	switch(type)
 	{
 		case 0:
-			spaces = 20 - buffer.size();
+			spaces = SIZE_NOMBRE - buffer.size();
 			break;
 		case 1:
-			spaces = 10 - buffer.size();
+			spaces = SIZE_CONTINENTE - buffer.size();
 			break;
 		case 2:
-			spaces = 9 - buffer.size();
+			spaces = SIZE_HABITANTES - buffer.size();
+			break;
+		case 3:
+			spaces = FECHA - buffer.size();
+			break;
+		case 4:
+			spaces = SIZE_CANTIDAD - buffer.size();
 			break;
 		default:
 			std::cout << "Error.\n";
@@ -72,18 +91,29 @@ void PrintFile(std::fstream& file,const std::string& buffer, PARAMETROS type)	//
 			//file << CalculateSpaces(buffer, type);
 			break;
 		case 2:
-			file << CalculateSpaces(buffer, type) + buffer + '\n';
+			file << CalculateSpaces(buffer, type) + buffer + " ";
+			file.flush();
+			//file << buffer;
+		case 3:
+			file << CalculateSpaces(buffer, type) + buffer + " ";
+			file.flush();
+			//file << buffer;
+		case 4:
+			file << CalculateSpaces(buffer, type) + buffer + " ";
 			file.flush();
 			//file << buffer;
 			break;
+		default:
+			std::cout << "Error.\n";
+			break;
 	}
-}
+} // Hay mucho codigo redundante aca
 
 
 
 void ImprimirPais()
 {
-	OpenFile(file, "paises.txt");
+	OpenFile(file, "Paises.txt");
 	std::string buffer;
 
 	std::cout << "Ingrese el nombre del pais: ";
@@ -98,22 +128,61 @@ void ImprimirPais()
 	std::cin >> buffer;
 	PrintFile(file, buffer, HABITANTES);
 
+	file << '\n';
+
+	CloseFile(file);
+}
+
+void ImprimirParte()
+{
+	OpenFile(file, "ParteDiario.txt");
+	std::string buffer;
+
+	std::cout << "Ingrese el nombre del pais: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, NOMBRE);
+
+	std::cout << "Ingrese el mes: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, FECHA);
+
+	std::cout << "Ingrese el dia: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, FECHA);
+
+	std::cout << "Ingrese la cantidad de hisopados: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, CANTIDAD);
+
+	std::cout << "Ingrese la cantidad de infectados: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, CANTIDAD);
+
+	std::cout << "Ingrese la cantidad de recuperados: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, CANTIDAD);
+
+	std::cout << "Ingrese la cantidad de fallecidos: ";
+	std::cin >> buffer;
+	PrintFile(file, buffer, CANTIDAD);
+
+	file << '\n';
 
 	CloseFile(file);
 }
 
 
 
-
-
 int main()
 {
 	int key;
-
+	
 	do
 	{
-		std::cout << "0 -\tCerrar programa.\n";
+		std::cout << "\n\n0 -\tCerrar programa.\n";
 		std::cout << "1 -\tIngresar datos de pais.\n";
+		std::cout << "2 -\tIngresar datos del parte diario.\n";
+
 		std::cout << "\nPor favor elija una opcion: ";
 
 		std::cin >> key;
@@ -125,13 +194,15 @@ int main()
 			case 1:
 				ImprimirPais();
 				break;
+			case 2:
+				ImprimirParte()
+				break;
 		}
 
-	}while(key != 0 || key < 0 || key > 1);
-
+	}while(key != 0 || key < 0 || key > 2);
+	
 
 	
 	
 	return 0;
 }
-
