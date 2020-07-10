c4ce6bd0cb185d26b8f75de1617463e4a2a2f06f
SachaCamiloGarcia <62697403+SachaCamiloGarcia@users.noreply.github.com>
2020-07-10 16:21:33 -0300

    Create ArbirCerrarArchivo.cpp
    Alguien se podria fijar si le compila este codigo en borland? Estoy teniendo problemas con el compilador. Tienen que tener un "archivo.txt" en el directorio raiz del codigo.

---
+++ Codigo/ArbirCerrarArchivo.cpp
@@ -0,0 +1,40 @@
+#include <fstream>
+#include <cassert>
+
+using namespace std;
+
+
+void AbrirArchivo(const char fileName[21], fstream& file)
+{
+	assert(fileName != NULL); 	// la cadena de caracteres no apunta a nada.
+	assert(!file.is_open());	// el archivo ya esta abierto o no existe en el directorio raiz.
+
+	file.open(fileName, fstream::in);
+
+	assert(file.is_open());		// el archivo no pudo abrise.
+}
+
+
+void CerrarArchivo(fstream& file)
+{
+	assert(file.is_open());		// el archivo que se quiere cerrar ya esta cerrado
+
+	file.close();
+
+	assert(!file.is_open());	// el archivo no pudo cerrarse
+}
+
+
+int main()    // main() esta solo de forma demostrativa.
+{
+	char fileName[21] = "archivo.txt";
+
+	//fstream file(fileName, fstream::in);
+	fstream file;
+	AbrirArchivo(fileName, file);
+	//file.close();
+	CerrarArchivo(file);
+
+
+	return 0;
+}
