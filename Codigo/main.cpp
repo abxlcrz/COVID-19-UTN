f86f59dd8c17ba84c4cd967768060aa8d2c25cad
SachaCamiloGarcia <62697403+SachaCamiloGarcia@users.noreply.github.com>
2020-07-12 10:35:14 -0300

    Create main.cpp

---
+++ Codigo/main.cpp
@@ -0,0 +1,21 @@
+#include <fstream.h>
+#include <iostream.h>
+#include <cstring.h>
+
+
+#include "encabezados.h"
+#include "intercambiar.h"
+#include "insertar.h"
+#include "shellSort.h"
+#include "archivos.h"
+#include "paises.h"
+#include "parteDiario.h"
+
+
+
+
+int main()
+{
+
+  return 0;
+}
