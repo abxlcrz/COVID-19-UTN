459d50d9e9f67fe86fc7e4b3189f6ff70504211e
SachaCamiloGarcia <62697403+SachaCamiloGarcia@users.noreply.github.com>
2020-07-12 03:42:17 -0300

    Update ProcesarPaises.cpp

--- Codigo/ProcesarPaises.cpp
+++ Codigo/ProcesarPaises.cpp
@@ -71,9 +71,7 @@ void ProcesarPaises()
 
     while(LeerPaises())
     {
-        strcpy(paises[counter].nombre, temp.nombre);
-        strcpy(paises[counter].continente, temp.continente);
-        paises[counter].habitantes = temp.habitantes;
+        paises[counter] = temp;
 
         //ImprimirPais(paises[counter]);
         counter++;
