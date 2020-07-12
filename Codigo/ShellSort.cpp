7ce508aed4978949df76d820773f30ae7df9cac6
SachaCamiloGarcia <62697403+SachaCamiloGarcia@users.noreply.github.com>
2020-07-12 09:12:25 -0300

    Create ShellSort.cpp

---
+++ Codigo/ShellSort.cpp
@@ -0,0 +1,42 @@
+enum PARAM
+{
+    NOMBRE = 10,
+    MES = 0,
+    DIA,
+    HISOPADOS,
+    INFECTADOS,
+    RECUPERADOS,
+    FALLECIDOS
+};
+
+void Shellsort(PARAM param, int size)
+{
+    PARTE temp;
+    int i = 0;
+    int k = 0;
+    int seq = 1;
+
+    while(seq <= size / 3)
+    {
+        seq = seq * 3 + 1;
+    };
+
+    while(seq > 0)
+    {
+        for(k = seq; k < size; k++)
+        {
+            temp = partes[k];
+            i = k;
+
+            while(i > seq - 1 && *(&partes[i - seq].mes + param) >= *(&temp.mes + param))
+            {
+                partes[i] = partes[i - seq];
+                i -= seq;
+            };
+
+            partes[i] = temp;
+        };
+
+        seq = (seq - 1) / 3;
+    }
+}
